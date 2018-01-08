25892af6a6c7a246482feb3095a231e4dee2466a
Rohit Swami <rowhitswami1@gmail.com>
2018-01-08 22:21:02 +0530

    Removed local address

--- application.py
+++ application.py
@@ -455,4 +455,4 @@ def showCategoryItemJSON(catalog_id, item_id):
 if __name__ == '__main__':
     app.debug = False
     app.secret_key = 'my_key'
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
